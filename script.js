c3a407bd98c15d964356c9962d3af17e18bbdffa
tomsza123 <szablak.tomek@gmail.com>
2019-08-05 21:55:12 +0200

    Add files via upload
    Udało się powiązać nazwy dań z piwkami. Metoda findBeer() pobiera nazwę dania(w założeniu nazwa ma być wysyłana po kliknięciu dania na stronie) i na jej podstawie przeszukuje listę dań we wszystkich piwkach dostępnych w api.

--- script.js
+++ script.js
@@ -1,19 +1,82 @@
 const apiurl =  "https://api.punkapi.com/v2/beers/ ";
-
 var request = new XMLHttpRequest()
-
 request.open('GET', apiurl, true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(beer => {
-      console.log(beer.name)
-    })
-  } else {
-    console.log('error')
-  }
+//$.getJSON(apiurl, function(data)
+
+function removeDuplicates(tab)//do przejrzenia
+{
+    //pętla usuwająca powtarzające się dania zebrane z piw(tab musi byc posortowana)
+    tab.sort();
+    for(var i=0;i<tab.length;i++)
+    {
+        if(tab[i++]=tab[i])
+        {
+            tab.splice(i,1);
+            i--;//wazna czesc kodu - po usunieciu petla musi wrócić na miejsce wczesniej zajmowane przez duplikat(bez tego petla pomija niektóre teoretyczne duplikaty)
+        }
+    }
+}
+function putToHtml(tab)
+{
+    for(var i=0;i<tab.length;i++)
+    {
+        document.write(tab[i] + "  " + i +"<br >");
+    }
 }
 
-request.send()
+request.onload = function() 
+{
+
+    const food_tab = [];
+    var beer_tab = [];
+    var data = JSON.parse(this.response)
+  
+    if (request.status >= 200 && request.status < 400) 
+    {
+        data.forEach(beer => 
+        {
+            beer_tab.push(beer.name);
+
+            for(var i=0;i<beer.food_pairing.length;i++)
+            {
+                food_tab.push(beer.food_pairing[i]);
+                //food_tab.push(beer.food_pairing[i]);
+            }
+        })
+    } 
+    else 
+    {
+        console.log('error')
+    }
+    
+    function findBeer(food)
+    {
+        if (request.status >= 200 && request.status < 400) 
+        {
+            data.forEach(beer => 
+            {
+                for(var i=0;i<beer.food_pairing.length;i++)
+                {
+                    if(beer.food_pairing[i] == food)
+                    {
+                        console.log(beer.name);
+                        console.log(beer.id)
+                    }
+                }
+            })
+        } 
+        else 
+        {
+            console.log('error')
+        }
+    }
+
+
+    putToHtml(food_tab);
+    findBeer("Yuzu cheesecake");
+      
+}
+request.send();
+
+
